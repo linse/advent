8d3d55c44e151d950862f7b9ffe7b6cc8d63c994
linse <sschirme@gmail.com>
2017-12-11 03:00:59 -0500

    More day 11.

--- 11/11.hs
+++ 11/11.hs
@@ -6,18 +6,20 @@ import Data.Bits
  
 main = do
   file <- readFile "input.txt"
-  putStrLn $ show $ solve $ words $ map (\c -> if c==',' then ' ' else c) file
-  putStrLn $ show $ maximum $ solve2 $ words $ map (\c -> if c==',' then ' ' else c) file
-  putStrLn $ show $ minimum $ solve2 $ words $ map (\c -> if c==',' then ' ' else c) file
+  let input = words $ map (\c -> if c==',' then ' ' else c) file
+  print $ solve input
+  print $ maximum $ solve2 input
+  print $ minimum $ solve2 input
 
 solve xs = res
   where 
-   n = length $ filter (\a -> a=="n") xs 
-   s = length $ filter (\a -> a=="s") xs 
-   nw = length $ filter (\a -> a=="nw") xs 
-   ne = length $ filter (\a -> a=="ne") xs 
-   sw = length $ filter (\a -> a=="sw") xs 
-   se = length $ filter (\a -> a=="se") xs 
+   count e xs = length $ filter (==e) xs
+   n  = count "n"  xs
+   s  = count "s"  xs
+   nw = count "nw" xs
+   ne = count "ne" xs
+   sw = count "sw" xs
+   se = count "se" xs
    n' = n - s
    nw' = nw - se
    ne' = ne - sw
@@ -25,15 +27,15 @@ solve xs = res
    ne'' = ne' - nw'
    res = n'' + ne'' -- count north component
 
-solve2 = solve' (0, 0, 0)
+solve2 = f (0, 0, 0)
   where 
-   solve' (n, ne, nw) [] = [(nw + n) + (ne - nw)]
-   solve' (n, ne, nw) (x:xs)
-    | x=="n" = sum:solve' (n+1, ne, nw) xs
-    | x=="s" = sum:solve' (n-1, ne, nw) xs
-    | x=="ne" = sum:solve' (n, ne+1, nw) xs
-    | x=="sw" = sum:solve' (n, ne-1, nw) xs
-    | x=="nw" = sum:solve' (n, ne, nw+1) xs
-    | x=="se" = sum:solve' (n, ne, nw-1) xs
-    | otherwise   = error $ show (n, ne, nw)
+   f (n, ne, nw) [] = [(nw + n) + (ne - nw)]
+   f (n, ne, nw) (x:xs)
+    | x=="n"  = sum:f (n+1, ne, nw) xs
+    | x=="s"  = sum:f (n-1, ne, nw) xs
+    | x=="ne" = sum:f (n, ne+1, nw) xs
+    | x=="sw" = sum:f (n, ne-1, nw) xs
+    | x=="nw" = sum:f (n, ne, nw+1) xs
+    | x=="se" = sum:f (n, ne, nw-1) xs
+    | otherwise = error $ show (n, ne, nw)
     where sum = (nw + n) + (ne - nw)
