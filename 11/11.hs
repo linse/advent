1ce27a4574023154b14c34876d6c79408b07ff60
linse <sschirme@gmail.com>
2017-12-11 23:28:39 -0500

    More day 11.

--- 11/11.hs
+++ 11/11.hs
@@ -1,33 +1,11 @@
-import System.Environment
-import Data.List
-import Data.Function
-import Data.Char
-import Data.Bits
- 
 main = do
   file <- readFile "input.txt"
   let input = words $ map (\c -> if c==',' then ' ' else c) file
-  print $ solve input
-  print $ maximum $ solve2 input
-  print $ minimum $ solve2 input
+  print $ last $ solve input
+  print $ maximum $ solve input
+  print $ minimum $ solve input
 
-solve xs = res
-  where 
-   count e xs = length $ filter (==e) xs
-   n  = count "n"  xs
-   s  = count "s"  xs
-   nw = count "nw" xs
-   ne = count "ne" xs
-   sw = count "sw" xs
-   se = count "se" xs
-   n' = n - s
-   nw' = nw - se
-   ne' = ne - sw
-   n'' = nw' + n' -- count nort component  
-   ne'' = ne' - nw'
-   res = n'' + ne'' -- count north component
-
-solve2 = f (0, 0, 0)
+solve = f (0, 0, 0)
   where 
    f (n, ne, nw) [] = [(nw + n) + (ne - nw)]
    f (n, ne, nw) (x:xs)
