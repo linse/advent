8e18f2ff39fa37d7e0b507c4661b1435f712b04a
sschirmer <sschirmer@etsy.com>
2017-12-14 18:28:33 -0500

    Day 13.

---
+++ 14/14.hs
@@ -0,0 +1,34 @@
+import Data.List (unfoldr)
+import Numeric (showHex, showIntAtBase)
+import Data.Char (intToDigit)
+import Data.Char (ord)
+import Data.Char (isSpace)
+import Data.Bits
+import Text.Printf
+ 
+main = do
+  let seq = [0 .. 255]
+  let filestring = zipWith (\a b-> a++"-"++show b) (repeat "jzgqcdpd") [0..127]--readFile "input.txt"
+  putStrLn $ solve seq filestring 
+
+solve seq string = show $length $filter (\a -> a=='1') $ concatMap (knotHash seq) $ string
+
+knotHash seq string = concatMap hex $ map dense $ blocks perm where
+  perm = hash seq (concat $ replicate 64 ascii)
+  ascii = (map ord $ concat $ lines $ string) ++ [17, 31, 73, 47, 23]
+
+  dense :: [Int] -> Int
+  dense = foldr xor 0
+  
+  blocks = splitEvery 16 where
+    splitEvery n = takeWhile (not . null) . unfoldr ( Just . splitAt n)
+  
+  hex = printf "%04b"
+
+hash seq lengths = hash' seq 0 0 lengths where
+  hash' perm pos ss []     = perm
+  hash' perm pos ss (x:xs) = hash' perm' pos' (ss + 1) xs where 
+      l = length perm
+      (rev, keep) = splitAt x $ take l $ drop pos $ cycle perm
+      perm' = take l $ drop (l - pos) $ cycle (reverse rev ++ keep)
+      pos' = (pos + x + ss) `mod` l
